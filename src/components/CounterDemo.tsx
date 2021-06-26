590bfa433eacf831513bf7d495b04fef1d74a629
Richard Ng <richardcrng@gmail.com>
2021-06-26 20:22:45 +0100

    Slight naming tweak to counterValueFromCurrentRender

--- src/components/CounterDemo.tsx
+++ src/components/CounterDemo.tsx
@@ -1,25 +1,25 @@
 import { useState } from "react";
 
 function CounterDemo(): JSX.Element {
-  const [currentRenderCounterValue, queueRerenderWithNewCounterValue] =
+  const [counterValueFromCurrentRender, queueRerenderWithNewCounterValue] =
     useState(11);
 
   const incrementCounter = () => {
-    const nextRenderValue = currentRenderCounterValue + 1;
-    console.log("current render value:", currentRenderCounterValue);
+    const nextRenderValue = counterValueFromCurrentRender + 1;
+    console.log("current render value:", counterValueFromCurrentRender);
     queueRerenderWithNewCounterValue(nextRenderValue);
     console.log(
       "queued a rerender with value of",
       nextRenderValue,
       "... although current render value is still",
-      currentRenderCounterValue
+      counterValueFromCurrentRender
     );
   };
 
   return (
     <>
       <h1>Counter Demo</h1>
-      <p>Current value: {currentRenderCounterValue}</p>
+      <p>Current value: {counterValueFromCurrentRender}</p>
       <button onClick={incrementCounter}>+1 to value</button>
     </>
   );
