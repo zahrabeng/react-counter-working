da983026cbbe779f8f273b0858b0c6f7f5101b9c
Richard Ng <richardcrng@gmail.com>
2021-06-27 18:27:51 +0100

    Add handle reset skeleton function

--- src/components/CounterDemo.tsx
+++ src/components/CounterDemo.tsx
@@ -2,19 +2,23 @@ import { useState } from "react";
 
 function CounterDemo(): JSX.Element {
   const [counterValueFromCurrentRender, queueRerenderWithNewCounterValue] =
-    useState(11);
+    useState(0);
 
   const handleAddOne = () => {
     queueRerenderWithNewCounterValue(counterValueFromCurrentRender + 1);
   };
 
-  console.log("Component is rendering with a counter value of", counterValueFromCurrentRender);
+  const handleReset = () => {
+    // TODO reset counter to 0 here
+  }
 
   return (
     <>
       <h1>Counter Demo</h1>
       <p>Current value: {counterValueFromCurrentRender}</p>
       <button onClick={handleAddOne}>+1</button>
+      <hr />
+      <button onClick={handleReset}>Reset counter</button>
     </>
   );
 }
